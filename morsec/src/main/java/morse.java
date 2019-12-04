15301cf3a6a5762a0ee13f2dbe62d1d5b3cba176
Dumi-web <57130669+Dumi-web@users.noreply.github.com>
2019-12-04 10:40:45 +0200

    Add files via upload
    Updated morsecode

---
+++ morsec/src/main/java/morse.java
@@ -0,0 +1,57 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class morse {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter message here");
+        String letters = userInput.nextLine();
+        System.out.println("Enter morse code here");
+        String Code = userInput.nextLine();
+        letters.toLowerCase();
+        Character [] alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '};
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","/"};
+        morseCode(alpha,morse,letters);
+        Words(morse,alpha,Code);
+    }
+    public static void morseCode(Character[] Words, String[] code, String input) {
+        ArrayList<String>converted = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            char Letter =input.charAt(i);
+
+            for (int j = 0; j<Words.length;j++) {
+                if (Words[j] == Letter) {
+                    converted.add(code[j]);
+                }
+
+
+            }
+
+        }
+        for(String list: converted){
+            System.out.print(list + " ");
+
+
+        }
+    }
+    public static void Words(String[]code, Character[] Words, String Code){
+        String[] array = Code.split(" ");
+        StringBuilder str = new StringBuilder();
+        for(int i =0; i < array.length;i++){
+            String Codec = array[i];
+            for(int j = 0; j < code.length;j++){
+                if(code[j].equals(Codec)){
+                    str.append(Words[j]);
+                }
+
+            }
+        }
+        System.out.print(str.toString());
+
+    }
+
+
+
+}
